8153fca23c48d112b20af9bd1f14ab4fb9b318c2
Don't Know <421826500@qq.com>
2023-05-17 15:00:45 +0800

    2023-5-17

--- public/open_templates/mybatis-plus/java/Dao.java
+++
@@ -1,9 +0,0 @@
-package  <%= variables.packageName %>.Dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import <%= variables.packageName %>.DO.<%= variables.className %>DO;
-
-
-public interface <%= variables.className %>Dao extends BaseMapper<<%= variables.className %>DO> {
-
-}
